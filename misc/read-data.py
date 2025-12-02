633fb2ed6112d0b5445304950d3759a76386d998
micedre <cedric.couralet@insee.fr>
2025-12-02 13:50:55 +0000

    Read with xr from http

--- misc/read-data.py
+++ misc/read-data.py
@@ -1,13 +1,5 @@
-from netCDF4 import Dataset
+import xarray as xr
 
+data = xr.open_dataset("https://object.files.data.gouv.fr/meteofrance-drias/SocleM-Climat-2025/EMULATEUR/METROPOLE/ALPX-12/MPI-ESM1-2-LR/r10i1p1f1/CNRM-ALADIN63-emul-CNRM-UNET11-tP22/historical/day/tasAdjust/version-hackathon-102025/tasAdjust_FR-Metro_MPI-ESM1-2-LR_historical_r10i1p1f1_CNRM_CNRM-ALADIN63-emul-CNRM-UNET11-tP22_v1-r1_MF-CDFt-ANASTASIA-SAFRAN-1985-2014_day_18500101-20141231.nc",  engine="h5netcdf")
 
-def walktree(top):
-    yield top.groups.values()
-    for value in top.groups.values():
-        yield from walktree(value)
-
-
-rootgrp = Dataset("tasAdjust_FR-Metro_MPI-ESM1-2-LR_historical_r10i1p1f1_CNRM_CNRM-ALADIN63-emul-CNRM-UNET11-tP22_v1-r1_MF-CDFt-ANASTASIA-SAFRAN-1985-2014_day_18500101-20141231.nc", "r")
-
-print(rootgrp.dimensions)
-rootgrp.close()
+print(data)
